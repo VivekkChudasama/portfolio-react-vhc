d0868d506183a2c7701aa027506e13da4ba9507d
vivekkchudasama <chudasamavivekk@yahoo.com>
2021-08-09 12:02:23 +0530

    Updates

---
+++ precache-manifest.6f4337c2a4be9ffcd386767b542dcb86.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae6c1f07bfd1112b84d1e5fd7d7591d",
+    "url": "/vhc-portfolio/index.html"
+  },
+  {
+    "revision": "b0747f6b669b7a02adad",
+    "url": "/vhc-portfolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "dd164ba9cfdac1759997",
+    "url": "/vhc-portfolio/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "e9fe7a36053f3ad88a2f",
+    "url": "/vhc-portfolio/static/css/main.46a03e1c.chunk.css"
+  },
+  {
+    "revision": "d7935a79f26671a37793",
+    "url": "/vhc-portfolio/static/js/2.edef2adf.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/vhc-portfolio/static/js/2.edef2adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0747f6b669b7a02adad",
+    "url": "/vhc-portfolio/static/js/3.e2b27656.chunk.js"
+  },
+  {
+    "revision": "dd164ba9cfdac1759997",
+    "url": "/vhc-portfolio/static/js/4.9b0241c4.chunk.js"
+  },
+  {
+    "revision": "e9fe7a36053f3ad88a2f",
+    "url": "/vhc-portfolio/static/js/main.b68246d9.chunk.js"
+  },
+  {
+    "revision": "7a525709813d6922265b",
+    "url": "/vhc-portfolio/static/js/runtime-main.502ee5f3.js"
+  },
+  {
+    "revision": "0a1caedbf378dede327627f938e9042b",
+    "url": "/vhc-portfolio/static/media/AecorLogo.0a1caedb.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/vhc-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/vhc-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "98c26263c3396f5119bfb01ecc40317a",
+    "url": "/vhc-portfolio/static/media/aiem.98c26263.png"
+  },
+  {
+    "revision": "13488c390ff70a4f88dcd63cb1163173",
+    "url": "/vhc-portfolio/static/media/akashTechnolabs.13488c39.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/vhc-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "9483ad0ef4cfe786a70165fdd63adbd4",
+    "url": "/vhc-portfolio/static/media/darshanlogo.9483ad0e.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/vhc-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "30626790a145dbcd57090cd255c9a587",
+    "url": "/vhc-portfolio/static/media/drsubhash.30626790.png"
+  },
+  {
+    "revision": "da6cec99310cb36142c10f91e264474b",
+    "url": "/vhc-portfolio/static/media/invennico.da6cec99.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/vhc-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/vhc-portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/vhc-portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/vhc-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/vhc-portfolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "67bd8b37401a1a579f0e901babc55631",
+    "url": "/vhc-portfolio/static/media/udemy.67bd8b37.png"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/vhc-portfolio/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
